49c50a9a765af9aed419fdbe9319a6488ae9b767
clement <olumiclement@gmail.com>
2022-09-08 12:07:39 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/** My first c program */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
