b3593723f98b329c412f7d83c0d300d25e0303ef
clement <olumiclement@gmail.com>
2022-09-11 12:22:45 -0700

    commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,12 @@
  *  * main - entry point
  *   *
  *    * Return: alway returns 0
- *     *
- */
+*/
 int main(void)
 {
-		char a;
-
-			for (a = 'z'; a >= 'a' ; a--)
-						putchar(a);
-				putchar('\n');
-					return (0);
+	char a;
+	for (a = 'z'; a >= 'a' ; a--)
+	putchar(a);
+	putchar('\n');
+	return (0);
 }
